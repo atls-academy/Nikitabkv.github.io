41bb1d7a4b18785592462d9c01183103cd84614e
Nikita <nikitabakumenko@yandex.ru>
2023-05-18 16:40:37 +0300

    Revert "- small refactor"
    This reverts commit 8b7b1eabdf2567db5073484c8f24d6991f7cbd25.

--- auth/login.ts
+++ auth/login.ts
@@ -2,7 +2,7 @@ import {IUser} from './user'
 export const login = (username: string, password: string, credentials: Array<IUser>, currentUserData: IUser) => {
     if (currentUserData.isAuth) throw new Error(`You must log out before logging in as a new user`)
 
-    if (!credentials.some(user => user.username === username && user.password === password)) {
+    if (!credentials.find(user => user.username === username && user.password === password)) {
         return (`Invalid login or password`)
     }
 
