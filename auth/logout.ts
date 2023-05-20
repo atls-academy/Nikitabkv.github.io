1cf13a74eb262bc0101e2725df4633ee24819dc2
Nikita <nikitabakumenko@yandex.ru>
2023-05-20 11:13:49 +0300

    fix: removed unused constant "username"

--- auth/logout.ts
+++ auth/logout.ts
@@ -1,7 +1,7 @@
 import {User} from './user.interface'
 
 export const logout = (activeUser: User) => {
-    const {isAuth, username} = activeUser
+    const {isAuth} = activeUser
 
     if (!isAuth) throw new Error('User not authorized')
 
